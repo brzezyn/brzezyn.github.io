01e9753668d3dd37c4c4ddcc1dfa1750e4e54e8f
Artur Brzeziński <arturitu.xdd@gmail.com>
2021-05-24 10:30:43 +0200

    URL fixed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-   const apiRoot = 'https://stormy-journey-40014.herokuapp.com/v1/task/';
+   const apiRoot = 'https://stormy-journey-40014.herokuapp.com/v1/';
    const trelloApiRoot = 'https://stormy-journey-40014.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
